d5487cf0e6fdd93ec90a1325ee36cd1124586218
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-30 15:15:32 -0700

    controllers connected to server and index page working correctly

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* == External Modules == */
+/* === External Modules === */
 const express = require('express');
 
 // module instance
@@ -6,5 +6,12 @@ const app = express();
 
 // PORT
 const PORT = 4000;
+
+/* === Internal Modules === */
+const controllers = require('./controllers/user_controllers');
+
+/* === Routes === */
+app.use('/', controllers);
+
 
 app.listen(PORT, () => console.log(`Ready to quelue, listening for client requests on port:`, PORT));
