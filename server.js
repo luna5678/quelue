6598f29fced494d22492781401335b7fd5c3e3a9
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-30 15:01:00 -0700

    set up basic server document and listener

---
+++ server.js
@@ -0,0 +1,10 @@
+/* == External Modules == */
+const express = require('express');
+
+// module instance
+const app = express();
+
+// PORT
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`Ready to quelue, listening for client requests on port ${PORT}`));
