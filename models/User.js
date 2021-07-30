4bfb5b1a102ef38fcf1320d3c8427db51950430e
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-30 15:53:05 -0700

    User schema created

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: [true, 'username required']
+        },
+        dob: {
+            type: Date,
+            required: [true, 'DOB required']
+        },
+        email: {
+            type: String,
+            required: [true, 'email required']
+        },
+        password: {
+            type: String,
+            required: [true, 'password required']
+        },
+        queue: {
+            type: [String],
+            default: []
+        },
+        showLikes: {
+            type: [mongoose.Shows.ObjectId],
+            ref: 'Show',
+        },
+        episodeLikes: {
+            type: [mongoose.Episodes.ObjectId],
+            ref: 'Episode'
+        }     
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
