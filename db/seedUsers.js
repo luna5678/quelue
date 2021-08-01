f2699f69c0a5fe434b5aa32e01c1bd5e7a9ee364
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-31 18:49:36 -0700

    finished seedUsers.js

--- db/seedUsers.js
+++ db/seedUsers.js
@@ -1,2 +1,27 @@
-const User = require('../models');
-require('../config/db.connection.js');
+const { User } = require('../models');
+
+const seed = async () => {
+    try {
+        await User.deleteMany();
+        const newUsers = await User.insertMany(
+            [
+                {
+                    username: 'LAsoul88',
+                    dob: new Date(1988, 2, 2),
+                    email: 'courtney.kakebeen@gmail.com',
+                    password: 'thisWorks'
+                },
+                {
+                    username: 'Luna5678',
+                    dob: new Date(2021, 0, 1),
+                    email: 'thisislonna@gmail.com',
+                    password: 'thisAlsoWorks'
+                },
+            ]);
+            console.log(newUsers);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+seed();
