61c0a089ee22aa1308e6bc710b22acce4ad61a3b
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-31 18:05:23 -0700

    adjusted show index route to return show objects

--- controllers/show_controllers.js
+++ controllers/show_controllers.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 const Show = require('../models/Show');
 
-router.get('/shows', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
         const allShows = await Show.find({});
         const context = {
             shows: allShows,
     };
-        res.send('data received', context); //NEED TO TEST SENDING SHOWS SEED DB
+        return res.send(context); //NEED TO TEST SENDING SHOWS SEED DB
     } catch (error) {
         console.log(error);
         req.error = error;
