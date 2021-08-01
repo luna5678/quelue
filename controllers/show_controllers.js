bd8b35896d5d2b0b56c22751b66ae194d3968b4d
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-31 18:12:26 -0700

    show show route up and running

--- controllers/show_controllers.js
+++ controllers/show_controllers.js
@@ -4,13 +4,29 @@ const router = express.Router();
 
 const Show = require('../models/Show');
 
+// index
 router.get('/', async (req, res, next) => {
     try {
         const allShows = await Show.find({});
         const context = {
             shows: allShows,
-    };
+        };
         return res.send(context); //NEED TO TEST SENDING SHOWS SEED DB
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+});
+
+// show
+router.get('/:id', async (req, res, next) => {
+    try {
+        const foundShow = await Show.findById(req.params.id);
+        const context = {
+            show: foundShow,
+        };
+        return res.send(context);
     } catch (error) {
         console.log(error);
         req.error = error;
