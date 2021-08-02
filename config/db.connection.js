52ee2ea4a6fa3388430e5d926de137f829b38fc6
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-02 09:59:20 -0700

    installed connect-mongo, dotenv, and express-session dependencies and required them all on server page; added session controller for atlas database

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionStr = 'mongodb://localhost:27017/quelue';
+const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/quelue';
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(connectionStr, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
