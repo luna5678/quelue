7389fe6371adf55de3e8d07cea646b758c194817
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-04 16:52:28 -0700

    commented out all of the episodes

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -10,8 +10,10 @@ const { User, Show, Episode } = require('../models');
 router.get('/:id', async (req, res, next) => {
     try {
         const foundShow = await User.findOne({ _id: req.params.id }).populate('showQueue');
+        // const foundEpisode = await User.findOne({ _id: req.params.id }).populate('episodeLikes');
         const context = {
             user: foundShow,
+            // user: foundEpisode
         };
         res.render('users/show.ejs', context);
     } catch (error) {
@@ -60,40 +62,38 @@ router.put('/:id', async (req, res, next) => {
     }
 });
 
-router.put('/:id', async (req, res, next) => {
-    try {
-        // Remove from like functionality
-        if (req.body.name == undefined) {
-            const updatedUser = await User.findOneAndUpdate(
-            { _id: req.session.currentUser.id },
-            { $pull: { episodeLikes: req.body.remove}}
-            ).populate('episodeLikes');
+// router.put('/:id', async (req, res, next) => {
+//     try {
+//         // Remove from like functionality
+//         if (req.body.name == undefined) {
+//             const updatedUser = await User.findOneAndUpdate(
+//             { _id: req.session.currentUser.id },
+//             { $pull: { episodeLikes: req.body.remove}}
+//             ).populate('episodeLikes');
             
-            return res.redirect(`/users/${req.session.currentUser.id}`); 
-    };
-    // Prevent duplication functionality
-    const foundUserEpisode = await User.findOne({ _id: req.session.currentUser.id });
-    console.log(foundUserEpisode);
-    const isInLikes = foundUserEpisode.episodeLikes.includes(req.body.name);
+//             return res.redirect(`/users/${req.session.currentUser.id}`); 
+//     };
+//     // Prevent duplication functionality
+//     const foundUserEpisode = await User.findOne({ _id: req.session.currentUser.id });
+//     const isInLikes = foundUserEpisode.episodeLikes.includes(req.body.name);
 
-    if (isInLikes) {
-        return;
-    }
+//     if (isInLikes) {
+//         return;
+//     }
 
-     // Add to likes functionality
-     const updatedUser = await User.findOneAndUpdate(
-        { _id: req.session.currentUser.id },
-        { $push: { episodeLikes: req.body.name }}
-        );
-        console.log('=====', req.body.name, '=====');
+//      // Add to likes functionality
+//      const updatedUser = await User.findOneAndUpdate(
+//         { _id: req.session.currentUser.id },
+//         { $push: { episodeLikes: req.body.name }}
+//         );
     
-    return res.redirect(`/users/${req.params.id}`);
+//     return res.redirect(`/users/${req.params.id}`);
 
-    } catch (error) {
-        console.log(error);
-        req.error = error;
-        return next();
-    }
-});
+//     } catch (error) {
+//         console.log(error);
+//         req.error = error;
+//         return next();
+//     }
+// });
 
 module.exports = router;
