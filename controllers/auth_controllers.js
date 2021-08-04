8ce5e11b8982f21878c9a0177a6bbc56bb45037f
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-03 22:15:09 -0700

    successful login now sends user to their homepage

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -5,53 +5,38 @@ const { User } = require('../models');
 
 // testing ejs pages for now. authentication is not yet set up
 
-// login GET
-router.get('/login', (req, res) => {
-    return res.render('auth/login')
-});
-
 // register GET
 router.get('/register', (req, res) => {
     return res.render('auth/register');
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const foundUser = await User.findOne({ email: req.body.email });
-        if (!foundUser) return res.redirect('/register');
-        const match = await bcrypt.compare(req.body.password, foundUser.password);
-        if (!match) return res.send('password invalid');
-        req.session.currentUser = {
-            id: foundUser._id,
-            username: foundUser.username,
-        };
-        return res.redirect('/products');
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
+// login GET
+router.get('/login', (req, res) => {
+    return res.render('auth/login')
 });
 
 // register POST
 router.post('/register', async (req, res) => {
     try {
-
+        
         if (req.body.password !== req.body.passwordTwo) {
             return res.send('passwords do not match')
         }
-
-        const foundUser = await User.exists({ $or: [{ email: req.body.email }, { username: req.body.username }], 
+        
+        const foundUser = await User.exists({ 
+            $or: [{ email: req.body.email }, { username: req.body.username }], 
         });
         if (foundUser) {
+            console.log(req.body.username);
             return res.redirect('/login');
         }
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(req.body.password, salt);
-
+        
         req.body.password = hash;
-
+        
         const newUser = await User.create(req.body);
-
+        
         return res.redirect('/login');
     } catch (error) {
         console.log(error);
@@ -59,4 +44,28 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const foundUser = await User.findOne({ username: req.body.username });
+        if (!foundUser)  {
+            console.log(req.body.username);
+            return res.redirect('/register');
+        }
+
+        const match = await bcrypt.compare(req.body.password, foundUser.password);
+        if (!match)  { 
+            console.log(match);
+            return res.send('password invalid');
+        }
+
+        req.session.currentUser = {
+            id: foundUser._id,
+            username: foundUser.username,
+        };
+        return res.redirect(`/users/${foundUser._id}`);
+    } catch (error) {
+        console.log(error);
+        return res.send(error);
+    }
+});
 module.exports = router;
