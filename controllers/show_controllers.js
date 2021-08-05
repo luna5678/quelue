d5dbb55deb64aee77411798d11e412f59fa640e8
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-05 11:36:15 -0700

    removed comments and organized code in show_controllers

--- controllers/show_controllers.js
+++ controllers/show_controllers.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const { nextTick } = require('process');
 const router = express.Router();
-
 const { User, Show, Episode } = require('../models');
 
-// index - for page with full Shows list
 router.get('/', async (req, res, next) => {
     try {
+
         const allShows = await Show.find({});
         const context = {
             shows: allShows,
         };
+
         return res.render('shows/index', context);
+
     } catch (error) {
         console.log(error);
         req.error = error;
@@ -19,23 +20,18 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-// show - for page with an individual Show displayed
 router.get('/:id', async (req, res, next) => {
     try {
+
         const foundShow = await Show.findById(req.params.id);
-        // episodeList = find episodes with same parentShow id
         const episodeList = await Episode.find({ parentShow: req.params.id });
-        // const user = await User.findById('61087e63a52d95573687ff93');
-        // user.showQueue = foundShow._id;
-        // add episodeList to context
-        // 
-
-
         const context = {
             show: foundShow,
             episodes: episodeList,
         };
+
         return res.render('shows/show', context);
+        
     } catch (error) {
         console.log(error);
         req.error = error;
