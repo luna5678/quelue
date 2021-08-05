61ac2121a791499ef52ad36a8e718575fcd06838
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-04 21:54:15 -0700

    deleted experimental code

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const functions = require('../public/scripts/app');
 const { User, Show, Episode } = require('../models');
 
 // register GET
