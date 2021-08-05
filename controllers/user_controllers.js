853770b972742bc023564e6acc794dbcd2c7b5d1
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-05 11:38:37 -0700

    removed comments, removed unused code, and organized code in user_controllers

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
 const { User, Show, Episode } = require('../models');
 
-
-/* === Routes === */
-
-// USER SHOW ROUTE TO DISPLAY PERSONAL QUEUES
 router.get('/:id', async (req, res, next) => {
     try {
+        
         const foundShow = await User.findOne({ _id: req.params.id }).populate('showQueue');
-        // const foundEpisode = await User.findOne({ _id: req.params.id }).populate('episodeLikes');
         const context = {
             user: foundShow,
-            // user: foundEpisode
         };
+
         res.render('users/show.ejs', context);
+
     } catch (error) {
         console.log(error);
         req.error = error;
@@ -23,13 +19,9 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
-
-
-// USER UPDATE ROUTE FOR QUEUES
-
 router.put('/:id', async (req, res, next) => {
     try {
-        // Remove from queue functionality
+        
         if (req.body.name == undefined) {
             const updatedUser = await User.findOneAndUpdate(
             { _id: req.session.currentUser.id },
@@ -39,7 +31,6 @@ router.put('/:id', async (req, res, next) => {
             return res.redirect(`/users/${req.session.currentUser.id}`); 
         };
 
-        // Prevent duplication functionality
         const foundUserShow = await User.findOne({ _id: req.session.currentUser.id });
         const isInQueue = foundUserShow.showQueue.includes(req.body.name);
 
@@ -47,7 +38,6 @@ router.put('/:id', async (req, res, next) => {
             return;
         }
         
-        // Add to queue functionality
         const updatedUser = await User.findOneAndUpdate(
             { _id: req.session.currentUser.id },
             { $push: { showQueue: req.body.name }}
@@ -61,39 +51,5 @@ router.put('/:id', async (req, res, next) => {
         return next();
     }
 });
-
-// router.put('/:id', async (req, res, next) => {
-//     try {
-//         // Remove from like functionality
-//         if (req.body.name == undefined) {
-//             const updatedUser = await User.findOneAndUpdate(
-//             { _id: req.session.currentUser.id },
-//             { $pull: { episodeLikes: req.body.remove}}
-//             ).populate('episodeLikes');
-            
-//             return res.redirect(`/users/${req.session.currentUser.id}`); 
-//     };
-//     // Prevent duplication functionality
-//     const foundUserEpisode = await User.findOne({ _id: req.session.currentUser.id });
-//     const isInLikes = foundUserEpisode.episodeLikes.includes(req.body.name);
-
-//     if (isInLikes) {
-//         return;
-//     }
-
-//      // Add to likes functionality
-//      const updatedUser = await User.findOneAndUpdate(
-//         { _id: req.session.currentUser.id },
-//         { $push: { episodeLikes: req.body.name }}
-//         );
-    
-//     return res.redirect(`/users/${req.params.id}`);
-
-//     } catch (error) {
-//         console.log(error);
-//         req.error = error;
-//         return next();
-//     }
-// });
 
 module.exports = router;
