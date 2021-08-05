b1a30eb85f345d46bd8877cc2b8ac6d177ea296c
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-04 22:32:11 -0700

    added functioning logout button to navbar

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -82,6 +82,11 @@ router.get('/logout', async (req, res) => {
     }
 });
 
+// logout button - POST
+router.post('/logout', (req, res) => {
+    return res.redirect('/login');
+});
+
 // edit profile - GET
 router.get('/:id/edit', async (req, res) => {
     try {
